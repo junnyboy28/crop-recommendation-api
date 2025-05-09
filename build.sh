6e168c8c1c151d2f738c0a3dd9ae0736733ee70e
Jimil <jimilmandani28@gmail.com>
2025-05-09 09:14:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,9 +8,8 @@ pip install numpy==1.23.5  # Use older, more compatible version
 pip install scipy==1.10.1  # Add explicit scipy version
 pip install joblib==1.2.0
 pip install scikit-learn==1.3.0  # Use slightly older version
-pip install catboost==1.1.1  # Use slightly older version
+pip install catboost==1.2  # FIXED: Changed to available version
 pip install -r requirements.txt
-
 
 # Create templates directory if it doesn't exist
 mkdir -p templates
